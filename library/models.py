4354910a6a6b6af1e59274c9b9058f91bf3b1e0b
paulinal3 <paulinal3@outlook.com>
2022-01-04 23:20:03 -0500

    dropping table to add plot column

--- library/models.py
+++ library/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+# Create your models here.
 class KDrama(models.Model):
     title = models.CharField(max_length=100)
     genre = models.CharField(max_length=100)
-    plot = models.CharField
+    plot = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
--- library/views.py
+++ library/views.py
@@ -1,3 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
 from django.shortcuts import get_object_or_404, render
 from rest_framework.views import APIView
 from rest_framework.response import Response
