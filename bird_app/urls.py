bd1978fafe301631358d4ac64b043743d23e362b
devinrbopp <devinrbopp@gmail.com>
2022-01-04 18:43:33 -0700

    full crud added

--- bird_app/urls.py
+++ bird_app/urls.py
@@ -1,1 +1,7 @@
 from django.urls import path
+from .views import BirdsView, BirdDetailView
+
+urlpatterns = [
+    path('', BirdsView.as_view(), name='Birds'),
+    path('<int:pk>/', BirdDetailView.as_view(), name='Bird-detail')
+]
--- bird_app/views.py
+++ bird_app/views.py
@@ -1,5 +1,45 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.http import JsonResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-def index(request):
-    return HttpResponse('hewwo')
+from .serializers import BirdSerializer
+
+from .models import Bird
+
+class BirdsView(APIView):
+    def get(self, request):
+        birds = Bird.objects.all()
+        data = BirdSerializer(birds, many=True).data
+        return Response(data)
+
+    def post(self, request):
+        print(request.data)
+        bird = BirdSerializer(data=request.data)
+        if bird.is_valid():
+            bird.save()
+            return Response(bird.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(bird.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class BirdDetailView(APIView):
+    def get(self, request, pk):
+        # show one bird
+        bird = get_object_or_404(Bird, pk=pk)
+        data = BirdSerializer(bird).data
+        return Response(data)
+    def delete(self, request, pk):
+        # delete one bird
+        bird = get_object_or_404(Bird, pk=pk)
+        bird.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    def patch(self, request, pk):
+        # patch (update) a bird
+        bird = get_object_or_404(Bird, pk=pk)
+        updated_bird = BirdSerializer(bird, data=request.data)
+        if updated_bird.is_valid():
+            updated_bird.save()
+            return Response(updated_bird.data)
+        else:
+            return Response(updated_bird.errors, status=status.HTTP_400_BAD_REQUEST)
